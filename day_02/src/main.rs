b88d39efa7938b0aad206b762d3dddd1e0e3d1ae
Florian Reisecker <florian.reisecker@gmail.com>
2019-12-10 21:18:43 +0100

    Solve day 2 part two

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,37 +1,50 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-
-    println!("The result of part one is {}.", part_one(&input));
+    let input: Vec<usize> = fs::read_to_string("input.txt").unwrap()
+        .split(",")
+        .map(|slice| slice.parse::<usize>().unwrap())
+        .collect();
+    println!("The result of part one is {}.", part_one(input.clone()));
+    println!("The result of part two is {}.", part_two(input));
 }
 
-fn part_one(input: &str) -> i32 {
-    // parse input to numbers
-    let mut input: Vec<i32> = input.split(",")
-        .map(|slice| slice.parse::<i32>().unwrap())
-        .collect();
-    // sanitize input
-    input[1] = 12;
-    input[2] = 2;
-    // process data
+fn part_one(input: Vec<usize>) -> usize {
+    run_program(input, 12, 2)
+}
+
+fn part_two(input: Vec<usize>) -> usize {
+    for noun in 1..=99 {
+        for verb in 1..=99 {
+            let output = run_program(input.clone(), noun, verb);
+            if output == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    panic!("Could not find the correct solution.");
+}
+
+fn run_program(mut input: Vec<usize>, noun: usize, verb: usize) -> usize {
+    input[1] = noun;
+    input[2] = verb;
     let mut i = 0;
     loop {
         let opcode = input[i];
         match opcode {
             1 | 2 => {
-                let target_pos = input[i + 3] as usize;
-                let input_a = input[input[i + 1] as usize];
-                let input_b = input[input[i + 2] as usize];
+                let target_pos = input[i + 3];
+                let input_a = input[input[i + 1]];
+                let input_b = input[input[i + 2]];
                 if opcode == 1 {
                     input[target_pos] = input_a + input_b;
                 } else {
                     input[target_pos] = input_a * input_b;
                 }
-            },
+            }
             99 => {
                 break;
-            },
+            }
             _ => panic!("Encountering an unknown opcode means something went wrong.")
         }
         i += 4;
