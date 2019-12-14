c972ac1bbb5c0d3eb596a3b3e0b3ec6c8d96ec05
Florian Reisecker <florian.reisecker@gmail.com>
2019-12-14 15:08:40 +0100

    Solve day 03 part two

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -7,30 +7,44 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
     println!("The result of part one is {}.", part_one(&input));
+    println!("The result of part two is {}.", part_two(&input));
 }
 
 fn part_one(input: &String) -> i32 {
-    let mut point_count_map: HashMap<(i32, i32), i32> = HashMap::new();
+    let mut point_count_map: HashMap<(i32, i32), (i32, i32)> = HashMap::new();
     let central_port = (1, 1);
     draw_wires(input, &mut point_count_map, central_port);
 
     point_count_map.iter()
-        .filter(|(_, count)| **count == INTERSECTION_SYMBOL)
+        .filter(|(_, value)| (**value).0 == INTERSECTION_SYMBOL)
         .map(|(point, _)| *point)
         .map(|point| calc_manhattan_distance(central_port, point))
         .min()
         .expect("Could not compute minimum distance to central port.")
 }
 
-fn draw_wires(input: &String, mut point_count_map: &mut HashMap<(i32, i32), i32>, central_port: (i32, i32)) {
+fn part_two(input: &String) -> i32 {
+    let mut point_count_map: HashMap<(i32, i32), (i32, i32)> = HashMap::new();
+    let central_port = (1, 1);
+    draw_wires(input, &mut point_count_map, central_port);
+
+    point_count_map.iter()
+        .filter(|(_, value)| (**value).0 == INTERSECTION_SYMBOL)
+        .map(|(_, value)| value.1)
+        .min()
+        .expect("Could not compute combined intersection distance.")
+}
+
+fn draw_wires(input: &String, mut point_count_map: &mut HashMap<(i32, i32), (i32, i32)>, central_port: (i32, i32)) {
     let mut line_identity = 0;
+    let mut counter = 0;
     for line in input.lines() {
         line_identity += 1;
-        draw_wire(&mut point_count_map, line_identity, line, central_port)
+        draw_wire(&mut point_count_map, line_identity, line, central_port, counter)
     }
 }
 
-fn draw_wire(mut point_count_map: &mut &mut HashMap<(i32, i32), i32>, line_identity: i32, line: &str, mut current_coordinate: (i32, i32)) -> () {
+fn draw_wire(mut point_count_map: &mut &mut HashMap<(i32, i32), (i32, i32)>, line_identity: i32, line: &str, mut current_coordinate: (i32, i32), mut counter: i32) -> () {
     for coordinate_str in line.split(',') {
         let direction = coordinate_str.get(..1).expect("Could not slice direction.");
         let length: i32 = coordinate_str.get(1..coordinate_str.len()).expect("Could not slice length.")
@@ -43,19 +57,21 @@ fn draw_wire(mut point_count_map: &mut &mut HashMap<(i32, i32), i32>, line_ident
                 "U" => current_coordinate.1 += 1,
                 _ => panic!("Invalid direction character."),
             }
-            insert_or_increment_count(&mut point_count_map, current_coordinate, line_identity);
+            counter += 1;
+            insert_or_increment_count(&mut point_count_map, current_coordinate, line_identity, counter);
         }
     }
 }
 
-fn insert_or_increment_count(point_count_map: &mut HashMap<(i32, i32), i32>, coordinate: (i32, i32), line_identity: i32) {
-    if let Some(count) = point_count_map.get_mut(&coordinate) {
+fn insert_or_increment_count(point_count_map: &mut HashMap<(i32, i32), (i32, i32)>, coordinate: (i32, i32), line_identity: i32, counter: i32) {
+    if let Some(value) = point_count_map.get_mut(&coordinate) {
         // check identity for every line to avoid intersecting with itself
-        if *count != INTERSECTION_SYMBOL && *count != line_identity {
-            *count = INTERSECTION_SYMBOL;
+        if (*value).0 != INTERSECTION_SYMBOL && (*value).0 != line_identity {
+            (*value).0 = INTERSECTION_SYMBOL;
+            (*value).1 += counter;
         }
     } else {
-        point_count_map.insert(coordinate, line_identity);
+        point_count_map.insert(coordinate, (line_identity, counter));
     }
 }
 
